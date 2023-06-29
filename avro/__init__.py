d6af0a8d8dcd598949bab1325d22afec9a4f7235
HitBlast <hitblastlive@gmail.com>
2023-06-29 22:57:30 +0600

    bumped version

--- avro/__init__.py
+++ avro/__init__.py
@@ -30,7 +30,7 @@ SOFTWARE.
 
 
 # Set version information.
-__version_info__ = ('2023', '6', '12')  # YYYY / MM / DD
+__version_info__ = ('2023', '6', '29')  # YYYY / MM / DD
 __version__ = '.'.join(__version_info__)
 
 
