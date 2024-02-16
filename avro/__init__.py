1db1b513407f9fce17944a8c983dfa11927e7e0b
HitBlast <hitblastlive@gmail.com>
2024-02-17 00:24:49 +0600

    bumped version

--- avro/__init__.py
+++ avro/__init__.py
@@ -5,5 +5,5 @@
 from .main import *
 
 # Set version information.
-__version_info__ = ('2024', '2', '9')  # YYYY / MM / DD
+__version_info__ = ('2024', '2', '17')  # YYYY / MM / DD
 __version__ = '.'.join(__version_info__)
