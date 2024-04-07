8c94b062a51012359b7eaf01db079c3c0b4fe456
HitBlast <hitblastlive@gmail.com>
2024-04-07 12:23:57 +0600

    added clipboard functionality to CLI

--- avro/cli.py
+++ avro/cli.py
@@ -10,6 +10,7 @@ def main():
     # Import click but only if it's available.
     try:
         import click
+        import pyclip
     except ImportError:
         return print('In order to enable CLI, please install avro.py using: pip install avro.py[cli]')
 
@@ -26,13 +27,27 @@ def main():
     @cli.command('parse', help='Parse a given text to Bangla / Bengali.')
     @click.argument('text')
     @click.option('--bijoy', is_flag=True, help='Use Bijoy Keyboard format for parsing.')
-    def _parse(text: str, bijoy: bool = False):
+    @click.option('--from-clipboard', is_flag=True, help='Parse text from clipboard.')
+    def _parse(text: str, bijoy: bool = False, from_clipboard: bool = False):
+        if text.strip() == '' and from_clipboard:
+            text = pyclip.paste(text=True)
+
+            if not text:
+                return click.echo('No text found in the clipboard.')
+
         click.echo((avro.parse(text) if not bijoy else avro.parse(text, bijoy=True)))
 
     # usage: avro reverse <text>
     @cli.command('reverse', help='Reverse a given text to English.')
     @click.argument('text')
-    def _reverse(text: str):
+    @click.option('--from-clipboard', is_flag=True, help='Reverse text from clipboard.')
+    def _reverse(text: str, from_clipboard: bool = False):
+        if text.strip() == '' and from_clipboard:
+            text = pyclip.paste(text=True)
+
+            if not text:
+                return click.echo('No text found in the clipboard.')
+
         click.echo(avro.reverse(text))
 
     # Run the command.
