7e0b58aa4d0bda4243130d81d5984aa9b4097e67
HitBlast <hitblastlive@gmail.com>
2024-04-27 11:46:42 +0600

    bumped version to current date

--- avro/__init__.py
+++ avro/__init__.py
@@ -7,5 +7,5 @@ from .main import *
 # Set package information.
 __description__ = 'A modern Pythonic implementation of Avro Phonetic.'
 
-__version_info__ = ('2024', '4', '7')  # YYYY / MM / DD
+__version_info__ = ('2024', '4', '27')  # YYYY / MM / DD
 __version__ = '.'.join(__version_info__)
