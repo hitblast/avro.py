db479d2a14450e1dd99db949bdc33db19f011d66
HitBlast <hitblastlive@gmail.com>
2024-10-26 18:41:01 +0600

    bumped version to current date

--- avro/__init__.py
+++ avro/__init__.py
@@ -11,5 +11,5 @@ Licensed under the terms of the MIT License.
 from .main import *
 
 # Version information.
-__version_info__ = (2024, 10, 12)
+__version_info__ = (2024, 10, 26)
 __version__ = ".".join(map(str, __version_info__))
