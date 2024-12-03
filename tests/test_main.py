e7f3abd48bfcf547aa801110add7e90dce1401dc
HitBlast <hitblastlive@gmail.com>
2024-12-04 01:16:13 +0600

    added basic impl

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,22 +10,26 @@ from typing import NoReturn
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 
 # Import local modules.
+import pytest
+
 import avro
 from avro.core import config
 
 
 # Test functions for this file.
-def test_patterns_without_rules_from_config() -> NoReturn:
+@pytest.mark.asyncio
+async def test_patterns_without_rules_from_config() -> NoReturn:
     """
     Tests all patterns from config that don't have rules.
     """
 
     for pattern in config.DICT["avro"]["patterns"]:
         if "rules" not in pattern and pattern.get("find", None):
-            assert pattern["replace"] == avro.parse(pattern["find"])
+            assert pattern["replace"] == await avro.parse(pattern["find"])
 
 
-def test_patterns_without_rules_not_from_config() -> NoReturn:
+@pytest.mark.asyncio
+async def test_patterns_without_rules_not_from_config() -> NoReturn:
     """
     Tests all patterns not from config that don't have rules.
 
@@ -43,10 +47,11 @@ def test_patterns_without_rules_not_from_config() -> NoReturn:
     }
 
     for key, value in conjunctions.items():
-        assert key == avro.parse(value)
+        assert key == await avro.parse(value)
 
 
-def test_patterns_numbers() -> NoReturn:
+@pytest.mark.asyncio
+async def test_patterns_numbers() -> NoReturn:
     """
     Test patterns - numbers
     """
@@ -66,10 +71,11 @@ def test_patterns_numbers() -> NoReturn:
     }
 
     for key, value in numbers.items():
-        assert key == avro.parse(value)
+        assert key == await avro.parse(value)
 
 
-def test_patterns_punctuations() -> NoReturn:
+@pytest.mark.asyncio
+async def test_patterns_punctuations() -> NoReturn:
     """
     Tests patterns - punctuations
     """
@@ -81,11 +87,12 @@ def test_patterns_punctuations() -> NoReturn:
     }
 
     for key, value in punctuations.items():
-        assert key == avro.parse(value)
-        assert value == avro.reverse(key)
+        assert key == await avro.parse(value)
+        assert value == await avro.reverse(key)
 
 
-def test_patterns_with_rules_svaravarna() -> NoReturn:
+@pytest.mark.asyncio
+async def test_patterns_with_rules_svaravarna() -> NoReturn:
     """
     Test patterns - with rules - svaravarna / shoroborno (derived from Bengali)
     """
@@ -105,10 +112,11 @@ def test_patterns_with_rules_svaravarna() -> NoReturn:
     }
 
     for key, value in svaravarna.items():
-        assert key == avro.parse(value)
+        assert key == await avro.parse(value)
 
 
-def test_non_ascii() -> NoReturn:
+@pytest.mark.asyncio
+async def test_non_ascii() -> NoReturn:
     """
     Test processor response for non-ASCII characters.
     Parse function should return any non-ASCII characters that is passed to it.
@@ -123,22 +131,24 @@ def test_non_ascii() -> NoReturn:
     }
 
     for key, value in non_ascii.items():
-        assert key == avro.parse(value)
+        assert key == await avro.parse(value)
 
 
-def test_ascii() -> NoReturn:
+@pytest.mark.asyncio
+async def test_ascii() -> NoReturn:
     """
     Test processor response for ASCII characters.
     Reverse function should return any ASCII characters that is passed to it.
     """
 
-    assert "Avwg evsjvi gan MvB|" == avro.reverse("Avwg evsjvi গান MvB|")
-    assert "Avwg amar Avwg‡K wPiw`b GB banglay Lyu‡R cvB!" == avro.reverse(
+    assert "Avwg evsjvi gan MvB|" == await avro.reverse("Avwg evsjvi গান MvB|")
+    assert "Avwg amar Avwg‡K wPiw`b GB banglay Lyu‡R cvB!" == await avro.reverse(
         "Avwg আমার Avwg‡K wPiw`b GB বাংলায় Lyu‡R cvB!"
     )
 
 
-def test_words_with_punctuations() -> NoReturn:
+@pytest.mark.asyncio
+async def test_words_with_punctuations() -> NoReturn:
     """
     Test parsing and reversing of words with punctuations.
     """
@@ -151,79 +161,84 @@ def test_words_with_punctuations() -> NoReturn:
     }
 
     for key, value in test_words.items():
-        assert key == avro.parse(value)
-        assert value.lower() == avro.reverse(key)
+        assert key == await avro.parse(value)
+        assert value.lower() == await avro.reverse(key)
 
 
-def test_exceptions() -> NoReturn:
+@pytest.mark.asyncio
+async def test_exceptions() -> NoReturn:
     """
     Test parsing and reversing of exceptions.
     """
 
-    assert "আমি উইকিপিডিয়া আর ফেসবুক চালাই।" == avro.parse("ami Wikipedia ar Facebook calai.")
-    assert "ami Wikipedia ar Facebook chalai." == avro.reverse("আমি উইকিপিডিয়া আর ফেসবুক চালাই।")
+    assert "আমি উইকিপিডিয়া আর ফেসবুক চালাই।" == await avro.parse("ami Wikipedia ar Facebook calai.")
+    assert "ami Wikipedia ar Facebook chalai." == await avro.reverse("আমি উইকিপিডিয়া আর ফেসবুক চালাই।")
 
 
-def test_conversion_bijoy_func() -> NoReturn:
+@pytest.mark.asyncio
+async def test_conversion_bijoy_func() -> NoReturn:
     """
     Test conversion to Bijoy directly.
     """
 
     # Regular Conversion.
-    assert "Avwg evsjvq Mvb MvB;" == avro.to_bijoy("আমি বাংলায় গান গাই;")
+    assert "Avwg evsjvq Mvb MvB;" == await avro.to_bijoy("আমি বাংলায় গান গাই;")
     assert [
         "Avwg evsjvi Mvb MvB|",
         "Avwg Avgvi Avwg‡K wPiw`b GB evsjvq Lyu‡R cvB!",
-    ] == avro.to_bijoy("আমি বাংলার গান গাই।", "আমি আমার আমিকে চিরদিন এই বাংলায় খুঁজে পাই!")
+    ] == await avro.to_bijoy("আমি বাংলার গান গাই।", "আমি আমার আমিকে চিরদিন এই বাংলায় খুঁজে পাই!")
 
     # Fail-safe Conversion.
-    assert "Hello, World!" == avro.to_bijoy("Hello, World!")
+    assert "Hello, World!" == await avro.to_bijoy("Hello, World!")
 
 
-def test_conversion_unicode_func() -> NoReturn:
+@pytest.mark.asyncio
+async def test_conversion_unicode_func() -> NoReturn:
     """
     Test conversion to Unicode directly.
     """
 
     # Regular Conversion.
-    assert "আমি বাংলায় গান গাই;" == avro.to_unicode("Avwg evsjvh় Mvb MvB;")
+    assert "আমি বাংলায় গান গাই;" == await avro.to_unicode("Avwg evsjvh় Mvb MvB;")
     assert [
         "আমি বাংলার গান গাই।",
         "আমি আমার আমিকে চিরদিন এই বাংলায় খুঁজে পাই!",
-    ] == avro.to_unicode("Avwg evsjvi Mvb MvB|", "Avwg Avgvi Avwg‡K wPiw`b GB evsjvq Lyu‡R cvB!")
+    ] == await avro.to_unicode("Avwg evsjvi Mvb MvB|", "Avwg Avgvi Avwg‡K wPiw`b GB evsjvq Lyu‡R cvB!")
 
 
-def test_parse_sentences() -> NoReturn:
+@pytest.mark.asyncio
+async def test_parse_sentences() -> NoReturn:
     """
     Test parsing of sentences (Unicode).
     """
 
     # Default parsing.
-    assert "আমি বাংলায় গান গাই;" == avro.parse("ami banglay gan gai;")
+    assert "আমি বাংলায় গান গাই;" == await avro.parse("ami banglay gan gai;")
     assert [
         "আমি বাংলার গান গাই।",
         "আমি আমার আমিকে চিরদিন এই বাংলায় খুঁজে পাই।",
-    ] == avro.parse("ami banglar gan gai.", "ami amar amike cirodin ei banglay khu^je pai.")
+    ] == await avro.parse("ami banglar gan gai.", "ami amar amike cirodin ei banglay khu^je pai.")
 
     # Bijoy parsing.
-    assert "Avwg evsjvq Mvb MvB;" == avro.parse("ami banglay gan gai;", bijoy=True)
+    assert "Avwg evsjvq Mvb MvB;" == await avro.parse("ami banglay gan gai;", bijoy=True)
     assert [
         "Avwg evsjvi Mvb MvB|",
         "Avwg Avgvi Avwg‡K wPiw`b GB evsjvq Lyu‡R cvB!",
-    ] == avro.parse("ami banglar gan gai.", "ami amar amike cirodin ei banglay khu^je pai!", bijoy=True)
+    ] == await avro.parse("ami banglar gan gai.", "ami amar amike cirodin ei banglay khu^je pai!", bijoy=True)
 
 
-def test_reverse_sentences() -> NoReturn:
+@pytest.mark.asyncio
+async def test_reverse_sentences() -> NoReturn:
     """
     Test reversing of sentences (Unicode).
     """
 
     # Default reversing.
-    assert "ami banglay gan gai." == avro.reverse("আমি বাংলায় গান গাই।")
+    assert "ami banglay gan gai." == await avro.reverse("আমি বাংলায় গান গাই।")
     assert [
         "rohim, tomake korim dakche. ekhon ki rowna debe?",
         "rowna dile amake bole zew.",
-    ] == avro.reverse("রহিম, তোমাকে করিম ডাকছে। এখন কি রওনা দেবে?", "রওনা দিলে আমাকে বলে যেও।")
+    ] == await avro.reverse("রহিম, তোমাকে করিম ডাকছে। এখন কি রওনা দেবে?", "রওনা দিলে আমাকে বলে যেও।")
 
     # Bijoy reversing.
-    assert "ami banglar gan gai." == avro.reverse("Avwg evsjvi Mvb MvB|", from_bijoy=True)
+    assert "ami banglar gan gai." == await avro.reverse("Avwg evsjvi Mvb MvB|", from_bijoy=True)
