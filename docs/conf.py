61c057a010c63253707e795d1458d54955211d4d
hitblast <67194380+hitblast@users.noreply.github.com>
2024-12-04 19:02:21 +0000

    fixed formatting errors (ruff)

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'avro.py'
-copyright = '2024, HitBlast'
-author = 'HitBlast'
-release = '2024.12.5'
+project = "avro.py"
+copyright = "2024, HitBlast"
+author = "HitBlast"
+release = "2024.12.5"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
