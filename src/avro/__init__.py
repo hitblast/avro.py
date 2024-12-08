81b2c83a55d4e0f8d3a2840f3e1cb95d0f54ab44
HitBlast <hitblastlive@gmail.com>
2024-12-09 01:02:15 +0600

    updated links

--- src/avro/__init__.py
+++ src/avro/__init__.py
@@ -1,25 +1,26 @@
 # SPDX-License-Identifier: MIT
 
 
-"""avro.py: A modern Pythonic implementation of the popular Bangla typing software Avro Phonetic.
+"""A modern Pythonic implementation of the popular Bangla typing software Avro Phonetic.
 
-Overview:
----------
+Overview
+--------
 
 avro.py provides a fully fledged, batteries-included text parser which can
 parse, reverse and even convert English Roman script into its phonetic
 equivalent (unicode) of Bengali.
 
-Features:
----------
-    1. Simple-to-use syntax with functional approach.
-    2. Easily convert/parse/reverse English Roman script to Bengali phonetic and vice versa.
-    3. Fully asynchronous and `async`/`await` sugarcoated syntax compliant.
-    4. Synchronous alternative functions available for applications requiring minimal overhead.
-    5. Batteries included. Just write your text and let it do the rest.
+Features
+--------
 
-Examples:
----------
+1. Simple-to-use syntax with functional approach.
+2. Easily convert/parse/reverse English Roman script to Bengali phonetic and vice versa.
+3. Fully asynchronous and `async`/`await` sugarcoated syntax compliant.
+4. Synchronous alternative functions available for applications requiring minimal overhead.
+5. Batteries included. Just write your text and let it do the rest.
+
+Examples
+--------
 
 Please refer to the following link to view the examples:
 https://github.com/hitblast/avro.py/tree/main/examples
