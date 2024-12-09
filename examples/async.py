f7108f9d8186b2c799fddd557d4654a47275ee7c
HitBlast <hitblastlive@gmail.com>
2024-12-09 20:55:14 +0600

    updated examples and finalized markdown

--- examples/async.py
+++ examples/async.py
@@ -9,24 +9,26 @@ async def main() -> None:
     dummy_text = "amar sOnar bangla"  # Dummy text.
     print("Original English Text:", dummy_text)
 
-    parsed_text = await avro.parse(dummy_text)  # Parse the text to Bengali.
+    parsed_text = await avro.parse_async(
+        dummy_text
+    )  # Parse the text to Bengali.
     print("Parsed Unicode Output:", parsed_text)
 
     # We can parse it directly to Bijoy, or use the to_bijoy function to convert it.
     # Both should return the same result.
-    bijoy_text_direct = await avro.parse(dummy_text, bijoy=True)
-    bijoy_text_function = await avro.to_bijoy(parsed_text)
+    bijoy_text_direct = await avro.parse_async(dummy_text, bijoy=True)
+    bijoy_text_function = await avro.to_bijoy_async(parsed_text)
 
     if bijoy_text_direct == bijoy_text_function:
         print(f"Bijoy Output: {bijoy_text_direct}")
 
     # Now, we can return the Bijoy text to Unicode since we'd like the original output (assuming).
     # This should be the same as the old parsed_text variable.
-    unicode_text = await avro.to_unicode(bijoy_text_direct)
+    unicode_text = await avro.to_unicode_async(bijoy_text_direct)
     print("Reversed Unicode Output:", unicode_text)
 
     # Finally, we can reverse the Bengali text, back to English!
-    reversed = await avro.reverse(unicode_text)
+    reversed = await avro.reverse_async(unicode_text)
     print("Reversed English Output:", reversed)
 
 
